c3ea09d924b62bd1ce6e8dce89ad4db5ec0efc43
alexander.andriishin <alexander@andriishin.com>
2024-04-02 11:29:47 +0300

    Added methods to run throwing exception code

--- src/main/java/com/nanolaba/sugar/Code.java
+++ src/main/java/com/nanolaba/sugar/Code.java
@@ -1,5 +1,46 @@
 package com.nanolaba.sugar;
 
+import java.util.function.Supplier;
+
 public interface Code {
 
+    static void run(CodeAction runnable) {
+        try {
+            runnable.run();
+        } catch (Exception t) {
+            throw new RuntimeException(t);
+        }
+    }
+
+    static <T> T run(CodeSupplier<T> runnable) {
+        try {
+            return runnable.get();
+        } catch (Exception t) {
+            throw new RuntimeException(t);
+        }
+    }
+
+    static <T> T runQuietly(CodeSupplier<T> runnable, Supplier<T> defaultObject) {
+        try {
+            return runnable.get();
+        } catch (Exception t) {
+            return defaultObject.get();
+        }
+    }
+
+    static void runQuietly(CodeAction runnable) {
+        try {
+            runnable.run();
+        } catch (Exception t) {/**/}
+    }
+
+    @FunctionalInterface
+    interface CodeAction {
+        void run() throws Exception;
+    }
+
+    @FunctionalInterface
+    interface CodeSupplier<T> {
+        T get() throws Exception;
+    }
 }
--- src/test/java/com/nanolaba/sugar/CodeTest.java
+++ src/test/java/com/nanolaba/sugar/CodeTest.java
@@ -2,10 +2,16 @@ package com.nanolaba.sugar;
 
 import org.junit.jupiter.api.Test;
 
+import static com.nanolaba.sugar.Code.run;
+import static com.nanolaba.sugar.Code.runQuietly;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SuppressWarnings({"DataFlowIssue", "unused"})
 class CodeTest {
 
     @Test
-    public void testCodeClassCanBeImplemented() {
+    public void testCodeInterfaceCanBeImplemented() {
 
         class ExtendedCode implements Code {
         }
@@ -13,4 +19,67 @@ class CodeTest {
         Code myCode = new ExtendedCode();
     }
 
+    @Test
+    public void testRunWithThrowingAnNPE() {
+        assertThrows(RuntimeException.class, () ->
+                run(() -> {
+                    Long t = null;
+                    long l = t / 10L;
+                })
+        );
+    }
+
+    @Test
+    public void testRunWithThrowingAnError() {
+        assertThrows(InternalError.class, () ->
+                run(() -> {
+                    throw new InternalError();
+                })
+        );
+    }
+
+    @Test
+    public void testRunWithThrowingAnException() {
+        assertThrows(RuntimeException.class, () ->
+                run(() -> {
+                    throw new Exception();
+                })
+        );
+    }
+
+    @Test
+    public void testRunSupplierWithThrowingAnException() {
+        assertThrows(RuntimeException.class, () -> {
+            class foo {
+                public long bar() {
+                    return run(() -> {
+                        Long t = null;
+                        return t / 10L;
+                    });
+                }
+            }
+
+            long test = new foo().bar();
+        });
+    }
+
+    @Test
+    public void testRunQuietlyAndCatchNPE() {
+        runQuietly(() -> {
+            Long t = null;
+            long l = t / 10L;
+        });
+    }
+
+    @Test
+    public void testRunQuietlyWithSupplierAndCatchNPE() {
+        long defaultValue = 100L;
+
+        Long v = runQuietly(() -> {
+            Long t = null;
+            return t / 10L;
+        }, () -> defaultValue);
+
+        assertEquals(defaultValue, (long) v);
+    }
 }
